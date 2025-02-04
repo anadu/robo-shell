320d7240784996621a4f14340fe61b1c732e6906
manoj <ankireddy828@gmail.com>
2025-02-04 11:30:47 +0530

    shell script

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,7 @@ dnf install nginx -y
 systemctl enable nginx 
 systemctl start nginx 
 rm -rf /usr/share/nginx/html/* 
+rm -rf /tmp/frontend.zip
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
