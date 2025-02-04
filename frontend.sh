04cbaf7546eb58aa62691663f2cc5c2d207b2ae1
manoj <ankireddy828@gmail.com>
2025-02-04 11:40:26 +0530

    location pathe

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,6 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
-cp nginx.conf  /etc/nginx/nginx.conf
 systemctl enable nginx 
 systemctl start nginx 
 rm -rf /usr/share/nginx/html/* 
@@ -9,4 +8,7 @@ rm -rf /tmp/frontend.zip
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+pwd
+ls -lrt
 systemctl restart nginx 
+cp nginx.conf  /etc/nginx/nginx.conf
