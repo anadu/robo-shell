4a6a2480da9267352d0c0915c2e8aa4bf58b9556
manoj <ankireddy828@gmail.com>
2025-02-04 16:33:12 +0530

    momgomessage

--- mongo.sh
+++ mongo.sh
@@ -2,5 +2,5 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y 
 systemctl enable mongod 
 systemctl start mongod 
-sed -i 's/27.0.0.1/0.0.0.0/'
+sed -i 's/27.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
