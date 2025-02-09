236c696866dfab9907917d8a4abe3b91cbfd2365
manoj <ankireddy828@gmail.com>
2025-02-09 16:00:42 +0530

    file

--- catalogue.sh
+++ catalogue.sh
@@ -7,13 +7,13 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 useradd roboshop
 rm -rf /app
 mkdir /app 
-curl -o /tmp/${app_name}.zip https://roboshop-artifacts.s3.amazonaws.com/${app_name}v3.zip 
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
 cd /app 
-unzip /tmp/${app_name}.zip
+unzip /tmp/catalogue.zip
 cd /app 
 npm install 
 systemctl daemon-reload
-systemctl enable ${app_name}
-systemctl restart ${app_name}
+systemctl enable $app_name
+systemctl restart $app_name
 dnf install mongodb-mongosh -y
 mongosh --host 10.0.0.5 </app/db/master-data.js
